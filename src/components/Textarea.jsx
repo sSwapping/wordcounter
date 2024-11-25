8ca2069fdde1d6a226ed4cf14bf1d5732547a4f3
SimpSwapping <154761856+SimpSwapping@users.noreply.github.com>
2024-11-25 18:06:41 +0100

    💄 Removed background color from Textarea component for improved styling

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -41,7 +41,7 @@ const Textarea = ({ text, setText }) => {
         onChange={handleChange}
         placeholder='Enter your text here...'
         spellCheck='false'
-        className='w-full h-64 p-4 text-lg resize-none focus:outline-none bg-neutral-100'
+        className='w-full h-64 p-4 text-lg resize-none focus:outline-none'
       />
       {warningText && <Warning warning={warningText} />}
     </div>
